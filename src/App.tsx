ab3b0f93928bd62c6614d089faa788bbc3bd1431
Maicon Gabriel Friedel <maicon.friedel@grupokyly.com.br>
2022-06-02 17:34:22 -0300

    Initial Commit

--- src/App.tsx
+++ src/App.tsx
@@ -1,25 +1,46 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { useState, useEffect } from "react";
+import "./App.css";
+
+import axios from "axios";
+
+import Header from "./components/Header";
 
 function App() {
+  const [counter, setCount] = useState({ count: 0 });
+
+  const [repos, setRepos] = useState([]);
+  const [loading, setLoading] = useState(false);
+
+  async function getData() {
+    setLoading(true);
+    const response = await axios.get(
+      "https://api.github.com/users/maiconfriedel/repos"
+    );
+
+    setRepos(response.data);
+    setLoading(false);
+  }
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <>
+      <Header />
+
+      <p>Você clicou {counter.count} vezes</p>
+      <button
+        onClick={() => {
+          getData();
+        }}
+      >
+        Incrementar Contagem
+      </button>
+
+      {loading && <p>Carregando...</p>}
+      <ul>
+        {repos.map((item: any) => {
+          return <li key={item.id}>{item.full_name}</li>;
+        })}
+      </ul>
+    </>
   );
 }
 
---
+++ src/components/Header.tsx
@@ -0,0 +1,13 @@
+import React from "react";
+
+import "./Header.css";
+
+function Header() {
+  return (
+    <div>
+      <p className="header">Kyly Indústria Têxtil LTDA</p>
+    </div>
+  );
+}
+
+export default Header;
